725c669bd046a0e2a32992fc9d4a76c67762c435
Katelyn Mirabelli <katelyn.mirabelli@gmail.com>
2020-02-12 14:28:32 -0500

    Index

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>INFO-638</title>
+</head>
+<link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap" rel="stylesheet">
+<style>
+	body {
+	background-color: #F1FAEE;
+	color: #1D3557;		
+	font-family: 'Catamaran', sans-serif;
+	font-size: 14px;
+	margin: 20px;
+	padding: 0;
+	}
+	h1 {
+	color: #E63946;
+	}
+	h2 {
+	color: #457B9D;
+	}
+	p {
+	font-size: 16px;
+	}
+</style>
+
+<h1>INFO-638</h1>
+
+<?php
+
+
+echo "<a href="#">Homework 1</a>";
+
+
+?>
+
+</body>
+</html>
