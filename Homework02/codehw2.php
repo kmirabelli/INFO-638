cb94881a4b552ca63bdb4ee8973a0f11f83bc37f
Katelyn Mirabelli <katelyn.mirabelli@gmail.com>
2020-02-12 17:47:45 -0500

    Homework 02 - initial

---
+++ Homework02/codehw2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Code Homework 2</title>
+</head>
+<link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap" rel="stylesheet">
+<style>
+	body {
+	background-color: #F1FAEE;
+	color: #1D3557;		
+	font-family: 'Catamaran', sans-serif;
+	font-size: 14px;
+	margin: 20px;
+	padding: 0;
+	}
+	h1 {
+	color: #E63946;
+	}
+	h2 {
+	color: #457B9D;
+	}
+	p {
+	font-size: 16px;
+	}
+</style>
+
+<h1>Challenge 1</h1>
+
+<?php
+
+echo "<p>Hello World!</p>";
+
+?>
+
+<h1>Challenge 2</h1>
+
+<?php
+
+echo "<p>Hello World!</p>";
+
+?>
+
+</body>
+</html>
